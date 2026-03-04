6c1fad02e8eda1324b44b6796fd439c38e9819d8
AD <70889070+Adstillcodes@users.noreply.github.com>
2026-03-04 08:43:43 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 export async function fetchMe() {
   const token = localStorage.getItem("token");
 
-  const res = await fetch("https://techfest-canada-frontend.vercel.app/api/auth/me", {
+  const res = await fetch("https://techfest-canada-frontend.vercel.app/api/auth", {
     headers: {
       Authorization: `Bearer ${token}`,
     },

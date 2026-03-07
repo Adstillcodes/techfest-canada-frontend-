7c5bd92c236e7164085d238d76a0d78ff53b101c
AD <70889070+Adstillcodes@users.noreply.github.com>
2026-03-07 14:32:47 +0530

    Update sanity.js

--- src/utils/sanity.js
+++ src/utils/sanity.js
@@ -1,17 +1,17 @@
 // src/utils/sanity.js
-import { createClient } from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url';
+
+import { createClient } from "@sanity/client";
+import { createImageUrlBuilder } from "@sanity/image-url";
 
 export const client = createClient({
-  // You can find your projectId in your Sanity Studio's sanity.cli.js file
-  projectId: '021qtoci', 
-  dataset: 'production',
-  useCdn: true, // Use the edge cache for fast loading
-  apiVersion: '2026-03-03', // Today's date
+  projectId: "021qtoci",
+  dataset: "production",
+  apiVersion: "2024-01-01",
+  useCdn: true,
 });
 
-// Set up the image builder
-const builder = imageUrlBuilder(client);
+// Correct image builder (new API)
+const builder = createImageUrlBuilder(client);
 
 export function urlFor(source) {
   return builder.image(source);

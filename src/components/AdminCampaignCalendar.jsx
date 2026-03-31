abcf687d556c5027d47d70117db01cb086e18db3
AD <anvit5.deshpande@gmail.com>
2026-03-31 11:15:59 +0530

    Error fix

--- src/components/AdminCampaignCalendar.jsx
+++ src/components/AdminCampaignCalendar.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import axios from "axios";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+import ReactQuill from "react-quill-new";
+import "react-quill-new/dist/quill.snow.css";
 
 const API = "https://techfest-canada-backend.onrender.com/api";
 
